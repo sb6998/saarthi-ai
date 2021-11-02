e9a576e2d474f44f829646ddd96f4017195084a6
sb6998 <saurabh.bhatia.phe17@iitbhu.ac.in>
2021-11-02 10:44:37 +0530

    Automatic configuration with django-heroku

--- saarthi/settings.py
+++ saarthi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
