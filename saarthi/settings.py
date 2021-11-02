cb2b1edacc3603b43d3aea6178b7bfa67a231fa0
sb6998 <saurabh.bhatia.phe17@iitbhu.ac.in>
2021-11-02 12:56:17 +0530

    Automate remote migrations

--- saarthi/settings.py
+++ saarthi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p8d=5*g)8m+%uazq-8zr3f*x_0rh*co*vzvxfb6t%$igai$&*j'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
